fefe4d1c785a4aa0e7ca2344b509426bb2312984
juanjoibar <juanjoibar@gmail.com>
2023-08-17 08:08:19 -0300

    vamos a usar Sass

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './app.scss'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='hello-scss'>Hello React</div>
+    </>
+  )
+}
+
+export default App
